974e05780d37ca6d35e3b0d2569e88bb46ab3456
naritabalqis <balqisnarita@gmail.com>
2024-10-04 22:22:01 +0700

    nambah fitur

--- Front-End/mwaa1/lib/Screen/profile_page.dart
+++ Front-End/mwaa1/lib/Screen/profile_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:mwaa1/widget/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -12,9 +14,39 @@ class ProfilePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.orange,
         leading: IconButton(
-            onPressed: () {}, 
-            icon: Icon(LineAwesomeIcons.angle_left_solid)),
-      
+            onPressed: () {},
+            icon: const Icon(LineAwesomeIcons.angle_left_solid)),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                const ClipOval(
+                  child: Image(
+                    image: AssetImage("profile.jpeg"),
+                    height: 180,
+                    width: 180,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: RichText(
+                    textAlign: TextAlign.start,
+                    text: TextSpan(children: [
+                      TextSpan(
+                          text: 'Nama \n',
+                          style: outfit17normal.copyWith(fontSize: 20)),
+                      TextSpan(
+                          text: 'Gmail',
+                          style: outfit17normal.copyWith(fontSize: 20))
+                    ]),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
