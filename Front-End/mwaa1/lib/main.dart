b41351f7e9b6f9a35fa856c0f7a256026dfe0334
bbbintang <zbintang18@gmail.com>
2024-11-08 19:27:12 +0700

    integrasi history page dnegan firebase

--- Front-End/mwaa1/lib/main.dart
+++ Front-End/mwaa1/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:mwaa1/Authentications/firebase_options.dart';
+import 'package:mwaa1/Screen/home_page.dart';
 import 'package:mwaa1/Screen/start_page.dart';
 
 Future<void> main() async {
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StartPage(),
+      home: HomePage(),
     );
   }
 }
